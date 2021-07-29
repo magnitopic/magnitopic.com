86d5f85da936652d653bdee1da1da68ae0da2c7e
magnitopic <magnitopic@gmail.com>
2021-07-29 20:06:20 +0200

    Header with my image

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,13 @@
+import Image from 'next/image';
 import styles from '../styles/Header.module.css';
 
 const Header= () => {
 	return (
 		<div id={styles.mainHeader}>
-			<a href="/"><h3>Magnitopic</h3></a>
+			<a href="/" id={styles.logo}>
+				<Image src='/../public/tierra_transparente.png' alt="Picture of the author"width="37" height="37"/>
+				<h3 id={styles.image} >Magnitopic</h3>
+			</a>
 			<div id={styles.links}>
 					<a class={styles.links} href=""><p>My work</p></a>
 					<a class={styles.links} href="/#contact"><p>Contact</p></a>
--- components/Two.js
+++ components/Two.js
@@ -1,11 +1,6 @@
 import styles from '../styles/Two.module.css';
 
 const Two= () => {
-
-	const next picture=()=>{
-		
-	}
-
 	return (
 		<div id={styles.two}>
 			<h1>Technologies I've worked with</h1>
