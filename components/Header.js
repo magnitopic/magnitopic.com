b8d092cffec6b3792647adbd7c68ff700be923bd
magnitopic <magnitopic@gmail.com>
2021-07-29 17:42:07 +0200

    Better header links

--- components/Header.js
+++ components/Header.js
@@ -5,9 +5,9 @@ const Header= () => {
 		<div id={styles.mainHeader}>
 			<a href="/"><h3>Magnitopic</h3></a>
 			<div id={styles.links}>
-				<a href="#"><p class={styles.p}>My work</p></a>
-				<a href="#"><p class={styles.p}>Contact</p></a>
-				<a href="https://altocodigo.blogspot.com/"><h4 class={styles.p}>Blogs</h4></a>
+					<a class={styles.links} href=""><p>My work</p></a>
+					<a class={styles.links} href="/#contact"><p>Contact</p></a>
+					<a class={styles.links} href="https://altocodigo.blogspot.com/"><h4>Blogs</h4></a>
 			</div>
 		</div>
 	)
