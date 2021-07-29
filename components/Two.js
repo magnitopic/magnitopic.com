4e253baa00f5e7c33b4b183251c07ac012ecb266
magnitopic <magnitopic@gmail.com>
2021-07-29 19:13:51 +0200

    Begining of two

---
+++ components/Two.js
@@ -0,0 +1,18 @@
+import styles from '../styles/Two.module.css';
+
+const Two= () => {
+
+	const next picture=()=>{
+		
+	}
+
+	return (
+		<div id={styles.two}>
+			<h1>Technologies I've worked with</h1>
+
+		</div>
+	)
+}
+
+
+export default Two
