664641e7234e4f58b1c6258cb1d1b6071076c4ac
magnitopic <magnitopic@gmail.com>
2021-08-02 20:51:23 +0200

    Added link to the header instead of a

--- components/Header.js
+++ components/Header.js
@@ -5,13 +5,15 @@ import styles from '../styles/Header.module.css';
 const Header= () => {
 	return (
 		<div id={styles.mainHeader}>
-			<a href="/" id={styles.logo}>
-				<Image src='/../public/tierra_transparente.png' alt="Picture of the author"width="37" height="37"/>
-				<h3 id={styles.pageName} >Magnitopic</h3>
-			</a>
+			<Link href="/" >
+				<a id={styles.logo}>
+					<Image src='/../public/tierra_transparente.png' alt="Picture of the author"width="37" height="37"/>
+					<h3 id={styles.pageName} >Magnitopic</h3>
+				</a>
+			</Link>
 			<div id={styles.links}>
 					<Link class={styles.links} href=""><p>My work</p></Link>
-					<Link class={styles.links} href="/#contact"><p>Contact</p></Link>
+					<Link class={styles.links} href=""><p>Contact</p></Link>
 					<Link class={styles.links} href="https://altocodigo.blogspot.com/"><h4>Blogs</h4></Link>
 			</div>
 		</div>
