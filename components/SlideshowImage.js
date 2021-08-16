daeb2952ea14cf09c695a3368fbef7fbcf3b04ae
magnitopic <magnitopic@gmail.com>
2021-08-16 20:13:16 +0200

    More Netlify deploy fixes

--- components/SlideshowImage.js
+++ components/SlideshowImage.js
@@ -2,11 +2,11 @@ import Image from 'next/image';
 
 function SlideshowImage(props){
 	return (
-		<div>
+		<>
 			{props.icons.map((images)=>(
-				<Image title={images.name} src={images.url} alt={images.name} height="100" width="100"/>
+				<Image key={images.id} title={images.name} src={images.url} alt={images.name} height="100" width="100"/>
 			))}
-		</div>
+		</>
 	)
 }
 
