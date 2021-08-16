0a09c111a37a0be91485a9ad783796bcd4062dac
magnitopic <magnitopic@gmail.com>
2021-08-16 20:08:33 +0200

    Netlify deploy fixes

--- components/Header.js
+++ components/Header.js
@@ -5,16 +5,16 @@ import styles from '../styles/Header.module.css';
 const Header= () => {
 	return (
 		<div id={styles.mainHeader}>
-			<Link href="/" >
+			<Link href="/" passHref>
 				<a id={styles.logo}>
 					<Image src='/tierra_transparente.png' alt="Picture of the author"width="45" height="45"/>
 					<h3 id={styles.pageName} >Magnitopic</h3>
 				</a>
 			</Link>
 			<div id={styles.links}>
-				<Link href=""><p className={styles.links}>My work</p></Link>
-				<Link href=""><p className={styles.links}>Contact</p></Link>
-				<Link href="https://altocodigo.blogspot.com/"><h4 className={styles.links}>Blogs</h4></Link>
+				<Link href="" passHref><p className={styles.links}>My work</p></Link>
+				<Link href="" passHref><p className={styles.links}>Contact</p></Link>
+				<Link href="https://altocodigo.blogspot.com/" passHref><h4 className={styles.links}>Blogs</h4></Link>
 			</div>
 		</div>
 	)
