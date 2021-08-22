1249ca88c817be35504ef637d2cb2d9afc43d979
magnitopic <magnitopic@gmail.com>
2021-08-23 01:43:43 +0200

    Fixed One's background colour

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,11 @@
 import Header from '../components/Header';
 import One from '../components/One';
 import Two from '../components/Two';
-import styles from '../styles/index.module.css'
 
 export default function index() {
 	return (
 		<>
-		<div id={styles.beginning}>
+		<div>
 			<Header/>
 			<One/>
 			<Two/>
