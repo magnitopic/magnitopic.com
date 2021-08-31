5bbcde0c10cd40fdbbfa6f8997d03aff5c72fd3f
magnitopic <magnitopic@gmail.com>
2021-08-31 13:18:56 +0200

    First wraper component

---
+++ components/Repos.js
@@ -0,0 +1,19 @@
+import Card from './ui/Card.js';
+import styles from '../styles/Repos.module.css'
+
+function Repos(){
+	
+
+	return (
+		<div className={styles.repo}>
+			<Card>
+			<p>Magnitopic</p>
+			</Card>
+			<Card>
+			<p>Magnitopic</p>
+			</Card>
+		</div>
+	)
+}
+
+export default Repos
--- components/Two.js
+++ components/Two.js
@@ -1,13 +1,16 @@
 import styles from '../styles/Two.module.css';
 import React, {useEffect} from 'react';
-import Slideshow from './Slideshow'
+import Slideshow from './Slideshow';
+import Repos from './Repos';
 
 
 const Two= () => {
 	return (
 		<div id={styles.two}>
+			
 			<h1>Technologies I&apos;ve worked with</h1>
 			<Slideshow/>
+			<Repos/>
 		</div>
 	)
 }
