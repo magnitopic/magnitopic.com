c4e90636f6b258a37e33a7b908d46fa7959e92c9
magnitopic <magnitopic@gmail.com>
2021-10-14 12:39:20 +0200

    Circle Scroll Button

--- components/Two.js
+++ components/Two.js
@@ -2,16 +2,25 @@ import styles from '../styles/Two.module.css';
 import React, {useEffect} from 'react';
 import Slideshow from './Slideshow';
 import Repos from './Repos';
+import CircleButton from './ui/CircleButton';
 
 
 const Two= () => {
 	return (
+		<>
+		<div
+		className={styles.downArow}
+		onClick={() => {
+			window.scrollTo({top: 1200,behavior: 'smooth'});
+		}}>
+			<CircleButton icon={"8595"} />
+		</div>
 		<div id={styles.two}>
-			
 			<h1>Technologies I&apos;ve worked with</h1>
 			<Slideshow/>
 			<Repos/>
 		</div>
+		</>
 	)
 }
 
