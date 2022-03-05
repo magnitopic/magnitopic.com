b99dcc96b7231a184be64d43f6e25e5d9430b86a
Magnitopic <magnitopic@gmail.com>
2022-03-05 22:40:28 +0100

    Fixed deployment bug

--- components/Meta.js
+++ components/Meta.js
@@ -4,7 +4,7 @@ const Meta= ({title, keywords, description}) => {
 	return (
 		<Head>
 			<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
-			<meta charset="UTF-8"></meta>
+			<meta charSet="UTF-8"></meta>
 			<meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
 			<meta name="keywords" content={keywords}/>
 			<meta name="description" content={description}/>
