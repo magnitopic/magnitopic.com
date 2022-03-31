045d12a30c1619af6cf3a20455eda83aec7ddeb0
magnitopic <magnitopic@gmail.com>
2022-03-31 19:48:54 +0200

    Creates more problems than it fixes

--- components/Slideshow.js
+++ components/Slideshow.js
@@ -10,7 +10,7 @@ const Slideshow = () => {
 
 	const [index, setIndex] = React.useState(0);
 	const timeoutRef = React.useRef(null);
-	const delay = 5000;
+	const delay = 2000;
 
 	//React functions to change the state of the Slider
 	function resetTimeout() {
@@ -24,7 +24,7 @@ const Slideshow = () => {
 		timeoutRef.current = setTimeout(
 			() =>
 				setIndex((prevIndex) =>
-					prevIndex === 3 - 1 ? 0 : prevIndex + 1
+					prevIndex === 2 - 1 ? 0 : prevIndex + 1
 				),
 			delay
 		);
@@ -34,28 +34,83 @@ const Slideshow = () => {
 		};
 	}, [index]);
 
-	const images=[
-			{name:'Python',url:'/tech_icons/python.png',height:100,width:100},
-			{name:'Java',url:'/tech_icons/java.png',height:120,width:120},
-			{name:'HTML',url:'/tech_icons/html.png',height:100,width:100},
-			{name:'CSS',url:'/tech_icons/css.png',height:100,width:100},
-			{name:'Java Script',url:'/tech_icons/JS.png',height:100,width:100},
-			{name:'jQuery',url:'/tech_icons/jquery.png',height:100,width:100},
-			{name:'Sass',url:'/tech_icons/sass.png',height:100,width:140},
-			{name:'React',url:'/tech_icons/react.png',height:100,width:100},
-			{name:'Node',url:'/tech_icons/node.png',height:100,width:100},
-			{name:'Express',url:'/tech_icons/express.png',height:100,width:300},
-			{name:'npm',url:'/tech_icons/npm.png',height:80,width:160},
-			{name:'Mongo DB',url:'/tech_icons/mongodb.png',height:100,width:300},
-			{name:'Markdown',url:'/tech_icons/markdown.png',height:90,width:120},
-			{name:'Raspberry Pi',url:'/tech_icons/rbpi.png',height:100,width:80},
-			{name:'Linux',url:'/tech_icons/linux.png',height:100,width:90},
-			{name:'Windows',url:'/tech_icons/windows.png',height:100,width:100},
-			{name:'Git',url:'/tech_icons/git.png',height:100,width:100},
-			{name:'Docker',url:'/tech_icons/docker.png',height:100,width:120},
-			{name:'Arduino',url:'/tech_icons/arduino.png',height:90,width:120},
-		];
-		
+	const images = [
+		{
+			name: "Python",
+			url: "/tech_icons/python.png",
+			height: 100,
+			width: 100,
+		},
+		{ name: "Java", url: "/tech_icons/java.png", height: 120, width: 120 },
+		{ name: "HTML", url: "/tech_icons/html.png", height: 100, width: 100 },
+		{ name: "CSS", url: "/tech_icons/css.png", height: 100, width: 100 },
+		{
+			name: "Java Script",
+			url: "/tech_icons/JS.png",
+			height: 100,
+			width: 100,
+		},
+		{
+			name: "jQuery",
+			url: "/tech_icons/jquery.png",
+			height: 100,
+			width: 100,
+		},
+		{ name: "Sass", url: "/tech_icons/sass.png", height: 100, width: 140 },
+		{
+			name: "React",
+			url: "/tech_icons/react.png",
+			height: 100,
+			width: 100,
+		},
+		{ name: "Node", url: "/tech_icons/node.png", height: 100, width: 100 },
+		{
+			name: "Express",
+			url: "/tech_icons/express.png",
+			height: 100,
+			width: 300,
+		},
+		{ name: "npm", url: "/tech_icons/npm.png", height: 80, width: 160 },
+		{
+			name: "Mongo DB",
+			url: "/tech_icons/mongodb.png",
+			height: 100,
+			width: 300,
+		},
+		{
+			name: "Markdown",
+			url: "/tech_icons/markdown.png",
+			height: 90,
+			width: 120,
+		},
+		{
+			name: "Raspberry Pi",
+			url: "/tech_icons/rbpi.png",
+			height: 100,
+			width: 80,
+		},
+		{ name: "Linux", url: "/tech_icons/linux.png", height: 100, width: 90 },
+		{
+			name: "Windows",
+			url: "/tech_icons/windows.png",
+			height: 100,
+			width: 100,
+		},
+		{ name: "Git", url: "/tech_icons/git.png", height: 100, width: 100 },
+		{
+			name: "Docker",
+			url: "/tech_icons/docker.png",
+			height: 100,
+			width: 120,
+		},
+		{
+			name: "Arduino",
+			url: "/tech_icons/arduino.png",
+			height: 90,
+			width: 120,
+		},
+	];
+
 	return (
 		<div className={styles.slideshow}>
 			<CircleButton
@@ -66,11 +121,13 @@ const Slideshow = () => {
 				}}
 			/>
 
-			<div
-				className={styles.slideshowImages}
-				style={{ transform: `translate3d(${-index * 35}%, 0, 0)` }}
-			>
-				<SlideshowImage images={images} />
+			<div>
+				<div
+					className={styles.slideshowImages}
+					style={{ transform: `translate3d(${-index * 35}%, 0, 0)` }}
+				>
+					<SlideshowImage images={images} />
+				</div>
 			</div>
 
 			<CircleButton
