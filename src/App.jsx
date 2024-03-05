126341afb4fc8a5fd5b39f872f0b4f3b65daadfc
magnitopic <magnitopic@gmail.com>
2024-03-05 13:01:26 +0100

    small style changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 				<div className="flex flex-1 justify-center">
 					<p className="text-purple-400 text-4xl font-bold">Magnitopic</p>
 				</div>
-				<div className="flex gap-12 align-middle justify-around flex-1 whitespace-nowrap">
+				<div className="flex gap-7 align-middle justify-around flex-1 whitespace-nowrap">
 					<p className="text-blue-300 text-xl font-bold underline flex cursor-pointer">
 						<a href="/#">About me</a>
 					</p>
@@ -40,7 +40,7 @@ function App() {
 						</p>
 					</div>
 				</div>
-				<div className="flex flex-wrap gap-6 p-14 bg-gradient-to-b from-sky-950 to-blue-500 pt-20">
+				<div className="flex flex-wrap gap-6 p-14 bg-gradient-to-b from-sky-950 to-blue-500">
 					<div className="bg-gray-900 rounded-md p-4 gap-4 flex flex-col flex-1">
 						<h1 className="text-3xl">minishell</h1>
 						<p>This project is about creating a simple shell. Yes, your own little bash. You will learn a lot about processes and file descriptors.</p>
