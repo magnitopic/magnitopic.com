24353cce760190fc42f16ac602cef5aec9491811
magnitopic <magnitopic@gmail.com>
2024-03-21 15:48:06 +0100

    new card element

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Card from "./components/Card.jsx";
+import CardVid from "./components/CardVid.jsx";
 import alaparic1 from "./img/alaparic.jpeg";
 import alaparic2 from "./img/tierra.png";
 import alaparic3 from "./img/spytf2.jpg";
@@ -15,31 +16,31 @@ function App() {
 	};
 
 	return (
-		<div className="text-blue-300 bg-blue-400">
+		<div className="text-white bg-blue-400">
 			<header className="flex flex-wrap gap-12 justify-around align-center p-4 bg-sky-950">
 				<div className="flex flex-1 justify-center">
-					<p className="text-purple-400 text-4xl font-bold">
+					<p className="text-purple-300 text-4xl font-bold">
 						Magnitopic
 					</p>
 				</div>
 				<div className="flex gap-7 align-middle justify-around flex-1 whitespace-nowrap">
-					<p className="text-blue-300 text-xl font-bold underline flex cursor-pointer">
+					<p className="text-xl font-bold underline flex cursor-pointer">
 						<a href="/#">About me</a>
 					</p>
-					<p className="text-blue-300 text-xl font-bold underline flex cursor-pointer">
+					<p className="text-xl font-bold underline flex cursor-pointer">
 						<a href="/#">Projects</a>
 					</p>
-					<p className="text-blue-300 text-xl font-bold underline flex cursor-pointer">
+					<p className="text-xl font-bold underline flex cursor-pointer">
 						<a href="/#">Activities</a>
 					</p>
-					<p className="text-blue-300 text-xl font-bold underline flex cursor-pointer">
+					<p className="text-xl font-bold underline flex cursor-pointer">
 						<a href="/#">Contact</a>
 					</p>
 				</div>
 			</header>
 			<main className="flex bg-sky-950 flex-col">
-				<div className="p-10 flex flex-row flex-1 flex-wrap">
-					<div className="flex flex-1 align-center justify-center">
+				<div className="p-10 flex flex-row flex-1 flex-wrap gap-6 justify-center">
+					<div className="flex flex-1 align-center justify-center min-w-60">
 						<img
 							src={currentImage}
 							alt="alaparic"
@@ -48,25 +49,42 @@ function App() {
 						/>
 					</div>
 					<div className="flex flex-col flex-1 justify-center">
-						<h1 className="text-5xl text-blue-300 font-bold">
+						<h1 className="text-5xl text-white font-bold text-purple-300">
 							Hello, I'm Alex
 						</h1>
-						<p className="text-2xl text-blue-300">
-							I'm a software developer and student at{" "}
-							<span className="italic">42Madrid</span>
+						<p className="text-2xl ">
+							I'm a software developer and student at 42Madrid
 						</p>
 					</div>
 				</div>
-				<div className="flex flex-wrap gap-6 p-14 bg-gradient-to-b from-sky-950 to-blue-500">
+				<div className="flex flex-wrap justify-center gap-14 p-14 bg-gradient-to-b from-sky-950 to-blue-500">
 					<Card
-						title="minishell"
+						title="Minishell"
 						text="This project is about creating a simple shell. Yes,
 							your own little bash. You will learn a lot about
 							processes and file descriptors."
+						image={alaparic3}
 					/>
-					<Card title="Rubber Ducky" text="" />
-					<Card title="cub3d" text="" />
-					<Card title="webserv" text="" />
+					<Card
+						title="Rubber Ducky"
+						text="Cybersecurity demonstration. Using a Rubber Ducky to change the DNS of a victim and redirect to a fake login form."
+						image={alaparic3}
+					/>
+					<Card
+						title="Cub3d"
+						text="Graphics project. Making a 3D game using ray tracing and programed in C. Like retro shooter games"
+						image={alaparic3}
+					/>
+					<Card
+						title="Webserv"
+						text="Web server with configurable behaviour. Implemented with non-blocking sockets, I/O Multiplexing and without threads."
+						image={alaparic3}
+					/>
+					<CardVid
+						title="Push-Swap"
+						text="Sort data on two stacks, with a limited set of instructions, using the lowest possible number of actions. Develop a sorting algorithm."
+						video="https://private-user-images.githubusercontent.com/21156058/247170625-a18ae060-f8a7-4b4e-a356-a4eb3c4e3793.mp4?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MTEwMjYzOTksIm5iZiI6MTcxMTAyNjA5OSwicGF0aCI6Ii8yMTE1NjA1OC8yNDcxNzA2MjUtYTE4YWUwNjAtZjhhNy00YjRlLWEzNTYtYTRlYjNjNGUzNzkzLm1wND9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDAzMjElMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwMzIxVDEzMDEzOVomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTAxMmU0ZmMwMGFjYjhlOWRjMDRkNmVhODVkMGYxOWQyYzEzZDMwMDViZDc3ZjY1YjYzY2EzOTM5N2RhOWIwMjkmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.IygtFDq2U3FmASEqrBcJNXfcygjqggc72p4PSmSgu1g"
+					/>
 				</div>
 			</main>
 		</div>
--- src/components/CardVid.jsx
+++ src/components/CardVid.jsx
@@ -1,23 +1,21 @@
-
-function Card({ title, text, image, link }) {
+function CardVid({ title, text, video, link }) {
 	return (
 		<div className="group flex flex-col justify-start items-start gap-2 w-96 h-56 duration-500 relative rounded-lg p-4 bg-gray-900 hover:-translate-y-2 hover:shadow-xl shadow-gray-800">
 			<div
 				alt="image here"
 				className="absolute duration-700 shadow-md group-hover:-translate-y-4 group-hover:-translate-x-4 -bottom-10 -right-10 w-1/2 h-1/2 rounded-lg bg-gray-800"
 			>
-				<img
-					src={image}
-					alt="project image"
-					className="rounded-lg w-full h-full object-cover object-top"
-				/>
+				<video className="rounded-lg h-60 w-60 object-cover object-top" controls>
+					<source src={video} type="video/mp4" />
+					Your browser does not support the video tag.
+				</video>
 			</div>
 
 			<div className="">
-				<h2 className="text-2xl font-bold mb-2 text-gray-100">{title}</h2>
-				<p className="text-gray-200 line-clamp-3">
-					{text}
-				</p>
+				<h2 className="text-2xl font-bold mb-2 text-gray-100">
+					{title}
+				</h2>
+				<p className="text-gray-200 line-clamp-3">{text}</p>
 			</div>
 			<button className="hover:bg-gray-700 bg-gray-800 text-gray-100 mt-6 rounded p-2 px-6">
 				Go to project →
@@ -26,4 +24,4 @@ function Card({ title, text, image, link }) {
 	);
 }
 
-export default Card;
+export default CardVid;
