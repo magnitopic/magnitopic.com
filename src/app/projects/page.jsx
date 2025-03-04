cb92dbc769f858adec2b4d6aeaaebc93370aaaa9
magnitopic <magnitopic@gmail.com>
2025-03-04 10:06:06 +0100

    more changes

---
+++ src/app/projects/page.jsx
@@ -0,0 +1,13 @@
+
+export const metadata = {
+    title: "Projects"
+}
+
+export default function Projects() {
+    return (
+        <main>
+            <div className="font-bold text-gray-600 text-2xl ">Projects</div>
+        </main>
+    );
+}
+
---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+const Header = () => {
+	return (
+		<div>
+			<h1>Testing</h1>
+
+			<nav>
+				<ul>
+					<li>Projects</li>
+					<li>About</li>
+					<li>Blog</li>
+				</ul>
+			</nav>
+		</div>
+	)
+}
+
+export default Header;
