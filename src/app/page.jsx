07046658aca8d13d84dc3a215d0cc009226506ea
magnitopic <magnitopic@gmail.com>
2025-08-24 14:27:06 +0200

    feat: new hero banner

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,18 +1,16 @@
-import BgCanvas from "../components/BgCanvas";
 import "font-awesome/css/font-awesome.min.css";
 import Link from "next/link";
-import { SocialsLinkButton } from "@/components/ui";
+import { SocialsLinkButton } from "../components/ui";
 
 export default function Home() {
 	return (
 		<>
-			<BgCanvas></BgCanvas>
-			<main className="flex flex-1 justify-around items-center flex-col z-10 relative bg-surface-primary text-content-primary">
-				<section className="container max-w-7xl px-4 flex-grow flex gap-5 justify-around flex-wrap min-h-screen items-center">
-					<div className="flex flex-col md:gap-5 gap-10 items-center md:items-start">
+			<main className="min-h-screen relative overflow-hidden items-center">
+				<section className="w-full px-4 flex-grow flex gap-5 flex-wrap min-h-screen items-center justify-center bg-image-[url('https://burakdogan.vercel.app/img/gradients/1.png')] bg-cover">
+					<div className="flex flex-col md:gap-5 gap-10 items-center">
 						<div>
-							<h1 className="md:text-5xl text-4xl max-w-xl md:text-start text-center mb-3">
-								Hi, I'm{" "}
+							<h1 className="md:text-5xl text-4xl max-w-xl text-center mb-3">
+								{"Hi, I'm "}
 								<span className="text-blue-300">
 									Alex Aparicio
 								</span>{" "}
@@ -21,7 +19,7 @@ export default function Home() {
 									Software developer
 								</span>
 							</h1>
-							<h2 className="text-xl md:text-start text-center">
+							<h2 className="text-xl text-center">
 								aka{" "}
 								<span className="text-blue-300">
 									Magnitopic
@@ -30,35 +28,34 @@ export default function Home() {
 						</div>
 						<div className="flex flex-row gap-5 text-3xl w-fit">
 							<SocialsLinkButton
-								link="https://twitter.com/magnitopic"
-								icon="fa fa-twitter"
+								link="https://github.com/magnitopic"
+								icon="fa fa-github"
 							/>
 							<SocialsLinkButton
 								link="https://www.linkedin.com/in/magnitopic/"
 								icon="fa fa-linkedin"
 							/>
 							<SocialsLinkButton
-								link="https://github.com/magnitopic"
-								icon="fa fa-github"
+								link="https://twitter.com/magnitopic"
+								icon="fa fa-twitter"
 							/>
 						</div>
-						<div className="text-center md:text-start">
+						<div className="text-center">
 							<p>
 								I love creating, learning and exploring
 								technology 👨‍💻
 							</p>
 							<p>
-								<span className="text-blue-200">
-									Web Development
-								</span>{" "}
-								-{" "}
-								<span className="text-blue-200">
-									Artificial Intelligence
+								<span className="text-blue-200 mt-10">
+									Web Development • Artificial Intelligence
 								</span>
 							</p>
 						</div>
 					</div>
 				</section>
+				{/* <section className="w-full px-4 flex-grow flex gap-5 flex-wrap min-h-screen items-center justify-center bg-gradient-to-t from-0% to-slate-900">
+					<h1 className="text-5xl">Skills</h1>
+				</section> */}
 				{/* <section className="container max-w-7xl my-10 px-4 flex-grow flex gap-5 flex-wrap">
 				<div>
 					<h1 className="text-5xl">
--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -1,29 +1,25 @@
-import BgCanvas from "../../components/BgCanvas";
 import ProjectCard from "../../components/ProjectCard";
 import projects from "../../../public/content/projects/cards.json";
 
 export default function Projects() {
 	return (
-		<>
-			<BgCanvas />
-			<main className="flex flex-1 justify-around items-center flex-col z-10 relative">
-				<section className="container max-w-7xl px-4 pt-32 pb-16">
-					<h1 className="text-5xl mb-8 text-center md:text-left">
-						<span className="text-blue-300">Projects</span>
-					</h1>
-					<p className="text-xl mb-12 max-w-3xl text-center md:text-left">
-						Here are some of the projects I've worked on. Click on a
-						project to learn more about it. You can also view the
-						project code on GitHub.
-					</p>
+		<main className="flex flex-1 justify-around items-center flex-col z-10 relative">
+			<section className="container max-w-7xl px-4 pt-32 pb-16">
+				<h1 className="text-5xl mb-8 text-center md:text-left">
+					<span className="text-blue-300">Projects</span>
+				</h1>
+				<p className="text-xl mb-12 max-w-3xl text-center md:text-left">
+					Here are some of the projects I've worked on. Click on a
+					project to learn more about it. You can also view the
+					project code on GitHub.
+				</p>
 
-					<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-						{projects.map((project) => (
-							<ProjectCard key={project.id} project={project} />
-						))}
-					</div>
-				</section>
-			</main>
-		</>
+				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+					{projects.map((project) => (
+						<ProjectCard key={project.id} project={project} />
+					))}
+				</div>
+			</section>
+		</main>
 	);
 }
